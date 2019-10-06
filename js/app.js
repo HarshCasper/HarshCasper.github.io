bfe4981fa621f16139d5b5dd68e2a60d34d3e105
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-10-06 08:13:57 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,119 +1,40 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+function setScrollingEvent(){
+    //Set Event Handling for navbar
+    $('.home_btn').click(function(){
+        $('#homepage').animatescroll({padding:50});
+    })
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
+    $('.about_btn').click(function(){
+        $('#aboutpage').animatescroll({padding:50});
+    })
 
-/* Otherwise just put the config content (json): */
+    $('.skills_btn').click(function(){
+        $('#skillspage').animatescroll({padding:50});
+    })
 
-particlesJS('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 50,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#6f8cf7"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#fff"
-        },
-        "polygon": {
-          "nb_sides": 5
-        }
-      },
-      "opacity": {
-        "value": 0.4,
-        "random": false,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0.2,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 6,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 2,
-          "size_min": 1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 140,
-        "color": "#999",
-        "opacity": 0.7,
-        "width": 0.8
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.4,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "bubble"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 250,
-          "line_linked": {
-            "opacity": 0.8
-          }
-        },
-        "bubble": {
-          "distance": 300,
-          "size": 5,
-          "duration": 1,
-          "opacity": 5,
-          "speed": 5
-        },
-        "repulse": {
-          "distance": 300
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-  }
-);
+    $('.resume_btn').click(function(){
+        $('#resumepage').animatescroll({padding:49});
+    })
+
+    // $('project_btn').click(function(){
+    //     $('#aboutpage').animatescroll({padding:50});
+    // })
+
+    $('.contact_btn').click(function(){
+        $('#contactpage').animatescroll({padding:50});
+    })
+
+    $('#logo-brand').click(function(){
+        $('#homepage').animatescroll();
+    })
+}   
+
+function init(){
+    /* affix the navbar after scrolling down */
+    $(".navbar").affix({offset: {top: 10} });
+    setScrollingEvent();
+}
+
+$(document).ready(function(){
+    init();
+})
