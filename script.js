3bc13995d8acd485cc7db26d67730a2eee5cff45
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-03-20 19:57:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const svg = document.querySelector('.svg');
+
+const colorSVG = () => {
+  svg.style.fill = '#ff8535';
+}
+
+const uncolorSVG = () => {
+  svg.style.fill = 'black';
+}
+
+const svg2 = document.querySelector('.svg2');
+
+const colorSVG2 = () => {
+  svg2.style.fill = '#ff8535';
+}
+
+const uncolorSVG2 = () => {
+  svg2.style.fill = 'black';
+}
+
+const svg3 = document.querySelector('.svg3');
+
+const colorSVG3 = () => {
+  svg3.style.fill = '#ff8535';
+}
+
+const uncolorSVG3 = () => {
+  svg3.style.fill = 'black';
+}
+
+
+
